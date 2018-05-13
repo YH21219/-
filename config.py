7de5915073aea07a44167bf86ce89533c001ec67
YH21219 <31643350+YH21219@users.noreply.github.com>
2018-05-13 23:37:52 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,52 @@
+header = {
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'Host': 'cas.dgut.edu.cn',
+    'Origin': 'https://cas.dgut.edu.cn',
+    'Referer': 'https://cas.dgut.edu.cn/home/Oauth/getToken/appid/jwxt.html',
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0',
+    'X-Requested-With': 'XMLHttpRequest',
+}
+
+header1 = {
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.170 Safari/537.36',
+    'Host': 'jwxt.dgut.edu.cn',
+}
+
+header_show = {
+    'Connection': 'keep-alive',
+    'Host': 'jwxt.dgut.edu.cn',
+    'Origin': 'http://jwxt.dgut.edu.cn',
+    'Referer': 'http://jwxt.dgut.edu.cn/dglgjw/student/xscj.stuckcj.jsp?menucode=JW130706',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.170 Safari/537.36',
+
+}
+
+headers = {
+    'Host': 'jwxt.dgut.edu.cn',
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Referer': 'http://jwxt.dgut.edu.cn/dglgjw/frame/Main_tools.jsp',
+    'Connection': 'close',
+}
+
+form_data = {
+    'sjxz': 'sjxz3',
+    'ysyx': 'yscj',
+    'zx': '1',
+    'fx': '1',
+    # 'userCode': '201500002986',
+    'xypjwchcnckcj': '0',
+    'pjwchckcjklpbcj': '0',
+    'xn': '2017',
+    'xn1': '2018',
+    'xq': '0',
+    'ysyxS': 'on',
+    'sjxzS': 'on',
+    'zxC': 'on',
+    'fxC': 'on',
+    'menucode_current': '',
+}
